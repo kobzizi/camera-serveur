774575613b95be3a02f6ae6a4fb7ee96fd32cceb
kobzizi <zizimail283@gmail.com>
2026-07-19 13:13:33 +0200

    Update server.py

--- server.py
+++ server.py
@@ -80,5 +80,5 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    print("Serveur démarré sur http://127.0.0.1:5000")
+    print("🚀 Serveur démarré sur Render")
     app.run(host='0.0.0.0', port=5000, debug=False)
